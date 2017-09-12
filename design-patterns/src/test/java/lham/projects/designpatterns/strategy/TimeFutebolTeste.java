f5746dce7848ecd60146cbf1440ef943f1ce2183
Luiz Mello <lhamello@gmail.com>
2017-09-11 23:52:13 -0300

    Implementado teste que garante que o time esteja atacando com a estratégia selecionda.

--- design-patterns/src/test/java/lham/projects/designpatterns/strategy/TimeFutebolTeste.java
+++ design-patterns/src/test/java/lham/projects/designpatterns/strategy/TimeFutebolTeste.java
@@ -7,16 +7,23 @@ public class TimeFutebolTeste {
 
 	@Test
 	public void devePossuirEstrategiaOfensiva() {
-		TimeFutebol time = new TimeFutebol(EstrategiaOfensiva.CHUVEIRINHO);
+		TimeFutebol time = new TimeFutebol(EstrategiaOfensiva.JOGAR_PELO_MEIO);
 		Assert.assertNotNull(time.getEstrategiaOfensiva());
 	}
-	
+
 	@Test
 	public void devePoderAlterarEstrategiaOfensiva() {
 		TimeFutebol time = new TimeFutebol(EstrategiaOfensiva.CHUVEIRINHO);
 		Assert.assertSame(EstrategiaOfensiva.CHUVEIRINHO, time.getEstrategiaOfensiva());
-		
+
 		time.alterarEstrategiaOfensiva(EstrategiaOfensiva.JOGAR_PELAS_LATERAIS);
 		Assert.assertSame(EstrategiaOfensiva.JOGAR_PELAS_LATERAIS, time.getEstrategiaOfensiva());
+	}
+
+	@Test
+	public void devePoderAtacarUtilizandoEstrategiaOfensivaAtual() {
+		TimeFutebol time = new TimeFutebol(EstrategiaOfensiva.JOGAR_PELO_LADO_DIREITO);
+
+		Assert.assertEquals("Atacando pelo lado direito.", time.atacar());
 	}
 }
